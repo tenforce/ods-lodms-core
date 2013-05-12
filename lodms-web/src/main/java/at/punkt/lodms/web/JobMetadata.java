38860fd2bc029b6e12a0bff0acc6aacaee5e23fb
nielsv <niels.vandekeybus@tenforce.com>
2013-05-12 10:46:13 +0200

    job meta data: renamed scheduled type to interval for clarity

--- lodms-web/src/main/java/at/punkt/lodms/web/JobMetadata.java
+++ lodms-web/src/main/java/at/punkt/lodms/web/JobMetadata.java
@@ -12,73 +12,73 @@ import java.util.Date;
  */
 public class JobMetadata implements Serializable {
 
-  static public final String T_SCHEDULED = "scheduled";
-  static public final String T_CHAINED = "chained";
-  private String name = "";
-  private String description = "";
-  private boolean scheduled;
-  private String interval = "";
-  private Date created = new Date();
-  private String previousJobId = "";
-  private String scheduleType = T_SCHEDULED;
+    static public final String T_SCHEDULED = "interval";
+    static public final String T_CHAINED = "chained";
+    private String name = "";
+    private String description = "";
+    private boolean scheduled;
+    private String interval = "";
+    private Date created = new Date();
+    private String previousJobId = "";
+    private String scheduleType = T_SCHEDULED;
 
-  public String getPreviousJobId() {
-    return previousJobId;
-  }
+    public String getPreviousJobId() {
+        return previousJobId;
+    }
 
-  public void setPreviousJobId(String previousJobId) {
-    this.previousJobId = previousJobId;
-  }
+    public void setPreviousJobId(String previousJobId) {
+        this.previousJobId = previousJobId;
+    }
 
-  public boolean isChained() {
-    return !previousJobId.isEmpty();
-  }
+    public boolean isChained() {
+        return !previousJobId.isEmpty();
+    }
 
-  public Date getCreated() {
-    return created;
-  }
+    public Date getCreated() {
+        return created;
+    }
 
-  public void setCreated(Date created) {
-    this.created = created;
-  }
+    public void setCreated(Date created) {
+        this.created = created;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-  public String getInterval() {
-    return interval;
-  }
+    public String getInterval() {
+        return interval;
+    }
 
-  public void setInterval(String interval) {
-    this.interval = interval;
-  }
+    public void setInterval(String interval) {
+        this.interval = interval;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public boolean isScheduled() {
-    return scheduled;
-  }
+    public boolean isScheduled() {
+        return scheduled;
+    }
 
-  public void setScheduled(boolean scheduled) {
-    this.scheduled = scheduled;
-  }
+    public void setScheduled(boolean scheduled) {
+        this.scheduled = scheduled;
+    }
 
-  public String getScheduleType() {
-    return scheduleType;
-  }
+    public String getScheduleType() {
+        return scheduleType;
+    }
 
-  public void setScheduleType(String scheduleType) {
-    this.scheduleType = scheduleType;
-  }
+    public void setScheduleType(String scheduleType) {
+        this.scheduleType = scheduleType;
+    }
 }
